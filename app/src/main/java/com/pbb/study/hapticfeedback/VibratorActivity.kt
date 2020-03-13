517a2ff20370ae934884ebd5632cf62d7f257f63
Issac Penn <pbbsignup@outlook.com>
2020-03-13 17:30:48 +0800

    predefined complete

--- app/src/main/java/com/pbb/study/hapticfeedback/VibratorActivity.kt
+++ app/src/main/java/com/pbb/study/hapticfeedback/VibratorActivity.kt
@@ -1,7 +1,15 @@
 package com.pbb.study.hapticfeedback
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Vibrator
+import android.os.VibrationEffect
+import android.view.View
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.core.content.getSystemService
 
 class VibratorActivity : AppCompatActivity() {
 
@@ -9,5 +17,20 @@ class VibratorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vibrator)
         this.title = "Vibrator"
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun performPredefinedVibration(view: View) {
+        val vibrator = applicationContext.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        if (view is Button) {
+            when (view.text) {
+                "EFFECT_TICK" -> vibrator.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK))
+                "EFFECT_CLICK" -> vibrator.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK))
+                "EFFECT_HEAVY_CLICK" -> vibrator.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK))
+                "EFFECT_DOUBLE_CLICK" -> vibrator.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_DOUBLE_CLICK))
+            }
+        }
+
     }
 }
